0326363c36bd9ee1040c7921bca654fa6569c1a5
nadimahmed1 <nadimarafat26@gmail.com>
2022-09-05 21:31:08 +0600

    font icon added

--- src/compunents/product/Product.js
+++ src/compunents/product/Product.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Product.css'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Product = (props) => {
 
@@ -14,7 +16,9 @@ const Product = (props) => {
                 <p><small>Sellar: {seller}</small></p>
                 <p><small>Ratings: {ratings}</small></p>
             </div>
-            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'><p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
         </div>
     );
 };
