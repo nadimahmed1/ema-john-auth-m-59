58efe97455eb8df8d8960a62073a5206b0192f1e
nadimahmed1 <nadimarafat26@gmail.com>
2022-09-05 17:40:38 +0600

    img setup

--- src/compunents/shop/Shop.js
+++ src/compunents/shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../product/Product';
 import './Shop.css'
 
 const Shop = () => {
@@ -8,12 +9,18 @@ const Shop = () => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [])
+    }, []);
 
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h3>this is for container {products.length}</h3>
+                {
+                    products.map(product => <Product
+
+                        key={product.id}
+                        product={product}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h4>Order summary </h4>
