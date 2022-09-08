8c891423414195645b67b0f9b3293f1122c71fa9
nadimahmed1 <nadimarafat26@gmail.com>
2022-09-08 18:03:28 +0600

    quantity problem solved

--- src/compunents/shop/Shop.js
+++ src/compunents/shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../product/Product';
 import './Shop.css'
@@ -13,10 +14,38 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
-    const handleAddToCart = (product) => {
-        console.log(product)
-        const newCart = [...cart, product];
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                console.log(addedProduct)
+                savedCart.push(addedProduct)
+            }
+            console.log(addedProduct)
+        }
+        setCart(savedCart)
+    }, [products])
+
+    const handleAddToCart = (selectedProduct) => {
+        // console.log(product)
+        let newCart = [];
+        const exixst = cart.find(product => product.id === selectedProduct.id);
+        if (!exixst) {
+            selectedProduct.quantity = 1
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exixst.quantity = exixst.quantity + 1;
+            newCart = [...rest, exixst]
+        }
+
         setCart(newCart)
+        addToDb(selectedProduct.id)
     }
 
     return (
